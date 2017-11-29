c596fc5f506790a9c8adf0a1798d81e24a6dae12
leanne63 <leanne63@users.noreply.github.com>
2017-11-29 10:22:21 -0600

    initial commit

---
+++ Example-Notifications/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Example-Notifications
+//
+//  Created by leanne on 11/29/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
