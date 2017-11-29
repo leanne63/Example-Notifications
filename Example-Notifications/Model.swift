9664f5901f44f665da0d938ac0395ed335193239
leanne63 <leanne63@users.noreply.github.com>
2017-11-29 11:35:05 -0600

    add basic notifications, prints to show process

---
+++ Example-Notifications/Model.swift
@@ -0,0 +1,43 @@
+//
+//  Model.swift
+//  Example-Notifications
+//
+//  Created by leanne on 11/29/17.
+//
+
+import Foundation
+
+class Model {
+	
+	// MARK: - Constants
+	
+	let notificationONEName = Notification.Name("NotificationONE")
+	let notificationTWOName = Notification.Name("NotificationTWO")
+	let notificationTHREEName = Notification.Name("NotificationTHREE")
+	
+	init() {
+		notifyObserversTWO()
+		NotificationCenter.default.post(name: notificationTWOName, object: self)
+	}
+	
+	
+	// MARK: - Observer-Related Methods
+	
+	func notifyObserversONE() {
+		print("START \(Model.self).\(#function)")
+		NotificationCenter.default.post(name: notificationONEName, object: self)
+		print("END \(Model.self).\(#function)")
+	}
+
+	func notifyObserversTWO() {
+		print("START \(Model.self).\(#function)")
+		NotificationCenter.default.post(name: notificationTWOName, object: self)
+		print("END \(Model.self).\(#function)")
+	}
+
+	func notifyObserversTHREE() {
+		print("START \(Model.self).\(#function)")
+		NotificationCenter.default.post(name: notificationTHREEName, object: self)
+		print("END \(Model.self).\(#function)")
+	}
+}
--- Example-Notifications/ViewController.swift
+++ Example-Notifications/ViewController.swift
@@ -8,15 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	// MARK: - Constants
+	let notificationONESelector = #selector(didReceiveNotificationONE)
+	let notificationTWOSelector = #selector(didReceiveNotificationTWO)
+	let notificationTHREESelector = #selector(didReceiveNotificationTHREE)
 
+	// MARK: - Properties
+	
+	let model = Model()
+	
+	
+	// MARK: - Lifecycle Overrides
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		subscribeToNotifications()
+		model.notifyObserversONE()
+		model.notifyObserversTWO()
 	}
+	
+	
+	// MARK: - Observer-Related Methods
+	
+	func subscribeToNotifications() {
+		NotificationCenter.default.addObserver(self,
+											   selector: notificationONESelector,
+											   name: model.notificationONEName,
+											   object: model)
+		
+		NotificationCenter.default.addObserver(self,
+											   selector: notificationTWOSelector,
+											   name: model.notificationTWOName,
+											   object: nil)
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+		NotificationCenter.default.addObserver(self,
+											   selector: notificationTHREESelector,
+											   name: model.notificationTHREEName,
+											   object: nil)
+	}
+	
+	@objc func didReceiveNotificationONE(notification: Notification) {
+		print("START \(ViewController.self).\(#function)")
+		exampleUtilityFunction()
+		print("END \(ViewController.self).\(#function)")
+	}
+	
+	@objc func didReceiveNotificationTWO(notification: Notification) {
+		print("START \(ViewController.self).\(#function)")
+		exampleUtilityFunction()
+		print("END \(ViewController.self).\(#function)")
+	}
+	
+	@objc func didReceiveNotificationTHREE(notification: Notification) {
+		print("START \(ViewController.self).\(#function)")
+		exampleUtilityFunction()
+		print("END \(ViewController.self).\(#function)")
+	}
+	
+	
+	// MARK: - Utility Functions
+	
+	func exampleUtilityFunction() {
+		print("START \(ViewController.self).\(#function)")
+		print("END \(ViewController.self).\(#function)")
 	}
 
 
